d790cb183b342aad7af9cc29e04cd587c519501c
winter <wantaotao1994@gmail.com>
2020-10-13 17:49:35 +0800

    代码格式化

---
+++ OutProxy/AbsClientOutProxy.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Winter.OutProxy
+{
+    public abstract class AbsClientOutProxy:IClientOutProxy
+    {
+        public abstract System.IO.Stream OutStream { protected set; get; }
+
+        public abstract Task ConnectAsync(byte[] buffer, int offset, int size, DomainModel doman);
+        public abstract void Dispose();
+        public abstract Task<byte[]> ReadDataAsync();
+        public abstract Task ReadDataAsync(Stream stream);
+        public abstract Task WriteDataAsync(byte[] buffer, int offset, int size);
+    }
+}
